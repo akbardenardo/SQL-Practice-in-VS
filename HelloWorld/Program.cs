a38629beee7805543a9da25b2dd80238d853e84e
opizo <opizo@OPIZOM>
2025-05-06 16:17:46 -0700

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        var connString = @"Server=(localdb)\MSSQLLocalDB;Database=DemoDB;Trusted_Connection=True;";
+        const string sql = "SELECT Id, FirstName, LastName FROM People";
+
+        using var conn = new SqlConnection(connString);
+        using var cmd = new SqlCommand(sql, conn);
+        cmd.Parameters.Add("@id", SqlDbType.Int).Value = 1;
+
+        conn.Open();
+        using var rdr = cmd.ExecuteReader();
+        while (rdr.Read())
+            Console.WriteLine($"{rdr["Id"]}: {rdr["FirstName"]} {rdr["LastName"]}");
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE People (
+	Id INT IDENTITY PRIMARY KEY,
+	FirstName NVARCHAR(50),
+	LastName NVARCHAR(50)
+);
+
+INSERT INTO People (FirstName, LastName)
+VALUES ('Akbar', 'Denardo'), ('Beenish', 'Farooqi');
